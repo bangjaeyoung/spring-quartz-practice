6d9a557bf6849b17879cb9568c4ba73e9940cb21
Jaeyoung Bang <qkdwo4613@gmail.com>
2024-04-14 22:42:25 +0900

    chore: remove unnecessary temporal annotation in entity

--- src/main/java/org/example/springquartzprac/item/domain/Item.java
+++ src/main/java/org/example/springquartzprac/item/domain/Item.java
@@ -4,14 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.sql.Date;
 import java.time.LocalDate;
 
 @Getter
@@ -27,8 +24,7 @@ public class Item {
     
     private Long price;
     
-    @Temporal(TemporalType.DATE)
-    private Date today = Date.valueOf(LocalDate.of(1999, 9, 8));
+    private LocalDate today = LocalDate.of(1999, 9, 8);
     
     @Builder
     private Item(String itemName, Long price) {
