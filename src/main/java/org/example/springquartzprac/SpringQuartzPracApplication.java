b069777fa19c3594850e83f52dca492ff097a7ff
Jaeyoung Bang <qkdwo4613@gmail.com>
2024-04-14 20:50:05 +0900

    init

---
+++ src/main/java/org/example/springquartzprac/SpringQuartzPracApplication.java
@@ -0,0 +1,13 @@
+package org.example.springquartzprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuartzPracApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringQuartzPracApplication.class, args);
+    }
+    
+}
